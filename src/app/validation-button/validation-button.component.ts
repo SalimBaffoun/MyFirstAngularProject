e20ea6bae1ff57b2d27066f40e23d702a51dbb70
Salim Baffoun <salim.baffoun@gmail.com>
2023-05-25 12:37:47 +0200

    renew title update

---
+++ src/app/validation-button/validation-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validation-button',
+  templateUrl: './validation-button.component.html',
+  styleUrls: ['./validation-button.component.css']
+})
+export class ValidationButtonComponent {
+
+}
